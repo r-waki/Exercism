d121774560180f3644bec93b087480f5f081a137
r-waki <r.wakeyii@gmail.com>
2021-09-14 20:32:38 +0900

    check function changes decorator

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,36 +1,32 @@
-def check_the_trianle(sides):
+def check_the_trianle(func):
+    def inner(sides):
+        if sum([a > 0 for a in sides]) != 3:
+            return False
 
-    if sum([a > 0 for a in sides]) != 3:
-        return False
+        if sides[0] + sides[1] <= sides[2]:
+            return False
 
-    if sides[0] + sides[1] <= sides[2]:
-        return False
+        if sides[0] + sides[2] <= sides[1]:
+            return False
 
-    if sides[0] + sides[2] <= sides[1]:
-        return False
+        if sides[1] + sides[2] <= sides[0]:
+            return False
 
-    if sides[1] + sides[2] <= sides[0]:
-        return False
+        return func(sides)
 
-    return True
+    return inner
 
 
+@check_the_trianle
 def equilateral(sides):
-    if check_the_trianle(sides):
-        return bool(len(set(sides)) == 1)
-
-    return False
+    return bool(len(set(sides)) == 1)
 
 
+@check_the_trianle
 def isosceles(sides):
-    if check_the_trianle(sides):
-        return bool(len(set(sides)) < 3)
-
-    return False
+    return bool(len(set(sides)) < 3)
 
 
+@check_the_trianle
 def scalene(sides):
-    if check_the_trianle(sides):
-        return bool(len(set(sides)) == 3)
-
-    return False
+    return bool(len(set(sides)) == 3)
