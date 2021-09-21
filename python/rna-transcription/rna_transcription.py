2da0bfb6a4155a60bedb388fda962625f3b00f96
r-waki <r.wakeyii@gmail.com>
2021-09-21 11:15:05 +0900

    [rna-transcription] use the translate method

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,17 +1,3 @@
 def to_rna(dna_strand):
-    if dna_strand == "":
-        return ""
-
-    dna_strand = list(dna_strand)
-    rna_strand = ""
-    for dna in dna_strand:
-        if dna == "C":
-            rna_strand = f"{rna_strand}G"
-        elif dna == "G":
-            rna_strand = f"{rna_strand}C"
-        elif dna == "T":
-            rna_strand = f"{rna_strand}A"
-        elif dna == "A":
-            rna_strand = f"{rna_strand}U"
-
-    return rna_strand
+    table = str.maketrans("CGTA", "GCAU")
+    return dna_strand.translate(table)
