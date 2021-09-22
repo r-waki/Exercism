c9a9f88f4f095204bbd9d42ef48462569c511014
r-waki <r.wakeyii@gmail.com>
2021-09-23 02:12:04 +0900

    [pig-latin] refactor & completed

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,27 +1,21 @@
 def translate(text):
+    return " ".join(translate_word(word) for word in text.split())
+
+
+def translate_word(word):
     vowel = ["a", "i", "u", "e", "o"]
-    words = text.split()
-    word_list = [list(x) for x in words]
 
-    translate_word = ""
+    while word[0] not in vowel:
+        if word[0:2] == "qu":
+            word = word[2:] + word[:2]
+            break
+        elif word[0:2] == "xr":
+            break
+        elif word[0:2] == "yt":
+            break
+        elif word[0] == "y" and word[1] not in vowel:
+            break
+        else:
+            word = word[1:] + word[0]
 
-    for word in word_list:
-        isConsonant = [x not in vowel for x in word]
-        for consonant in isConsonant:
-            if not consonant:
-                break
-            if word[0:2] == ["q", "u"]:
-                del word[0:2]
-                word.append("qu")
-            elif word[0:2] == ["x", "r"]:
-                break
-            elif word[0:2] == ["y", "t"]:
-                break
-            elif word[0] == "y" and word[1] not in vowel:
-                break
-            else:
-                word.append(word.pop(0))
-
-        translate_word += "".join(word) + "ay "
-
-    return translate_word.rstrip()
+    return word + "ay"
