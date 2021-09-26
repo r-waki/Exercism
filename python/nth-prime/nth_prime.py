71b477d9eabcd7cad158b3f141f6b9e9648c862e
r-waki <r.wakeyii@gmail.com>
2021-09-26 22:37:11 +0900

    [nth-prime] test clear but speed too late

---
+++ python/nth-prime/nth_prime.py
@@ -0,0 +1,21 @@
+def prime(number):
+    prime_numbers = prime_range(number)
+    return prime_numbers[number - 1]
+
+
+def prime_range(number):
+    if number <= 0:
+        raise ValueError("number is not essential number")
+    checked_number = 3
+    prime_numbers = [2]
+
+    while len(prime_numbers) != number:
+        for n in range(2, checked_number):
+            if checked_number % n == 0:
+                break
+            if checked_number-1 == n:
+                prime_numbers.append(checked_number)
+
+        checked_number += 1
+
+    return prime_numbers
