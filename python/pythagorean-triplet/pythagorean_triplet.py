7d152bedb4b01548f40278cf2fd538f7a523549a
r-waki <r.wakeyii@gmail.com>
2021-10-11 12:10:26 +0900

    [pythagorean-triplet] completed

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,7 +1,11 @@
+
 def triplets_with_sum(number):
+
     triplet = []
     for a in range(1, number):
-        for b in range(a, number):
+        for b in range(a + 1, number):
+            if number - a - b < b:
+                break
             if a ** 2 + b ** 2 == (number - a - b) ** 2:
                 triplet.append([a, b, number - a - b])
 
