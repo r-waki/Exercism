e08c1c362df2bfb5754968a29428d48131f4c3fa
r-waki <r.wakeyii@gmail.com>
2022-04-09 02:26:51 +0900

    py[rectangles]test success

--- python/rectangles/rectangles.py
+++ python/rectangles/rectangles.py
@@ -1,4 +1,5 @@
 import itertools
+import numpy as np
 
 
 def rectangles(strings):
@@ -7,16 +8,20 @@ def rectangles(strings):
     row_volume = len(strings)
     vertical_combinations = itertools.combinations(range(0, row_volume), 2)
 
-    vertex_coordinate = []
-
     # 任意の列の頂点となる + 座標位置を特定
+    column_number = 0
+    vertex_coordinate = []
     for row in strings:
         vertex_coordinate.append([i for i, e in enumerate(row) if e == "+"])
+        column_number = len(row)
 
-    vertical_line_element = []
-
-    for row in strings:
-        vertical_line_element.append(i for i, e in enumerate(row) if e =="+" or e == "|")
+    # 縦と横を入れ替えたリストを作成する
+    tr = []
+    for i in range(column_number):
+        tr_row = []
+        for vector in strings:
+            tr_row.append(vector[i])
+        tr.append(tr_row)
 
 
     # 縦線の組み合わせから、同じ位置に + があれば一つ長方形ができる
@@ -24,24 +29,18 @@ def rectangles(strings):
     for v in vertical_combinations:
         row_a = set(itertools.combinations(vertex_coordinate[v[0]], 2))
         row_b = set(itertools.combinations(vertex_coordinate[v[1]], 2))
- 
-        # vertical_line_coodinate = []
 
-        # if v[1] - v[0] != 1 :
-        #     for line_number in range( v[0]+1, v[1]+1):
-        #         vertical_line_coodinate.append(itertools.combinations(vertical_line_element[line_number],2))  
+        ab = (row_a & row_b)
 
-        #     vertical_line = {}
-        #     for i, vl in enumerate(vertical_line_coodinate):
-        #         if i == 0:
-        #             continue
-        #         elif i == 1:
-        #             vertical_line = vertical_line_coodinate[i-1] & set(vl)
-        #         else:
-        #             vertical_line = vertical_line & set(vl)
+        if v[1] - v[0] > 1:
 
-        #     square = square + len(row_a & row_b & vertical_line)
-        # else:
-        square = square + len(row_a & row_b)
+            for x in list(ab):
+                column_a = "".join(tr[x[0]])
+                column_b = "".join(tr[x[1]])
+                if all((y == "|" or y == "+") for y in column_a[v[0]+1:v[1]]) and\
+                   all((y == "|" or y == "+") for y in column_b[v[0]+1:v[1]]):
+                    square = square + 1
+        else:
+            square = square + len(ab)
 
     return square
