5a09c83c3f34c63c52f95af574f2746062ae5260
r-waki <r.wakeyii@gmail.com>
2023-12-02 23:43:16 +0900

    fix(python): test completed

--- python/diamond/diamond.py
+++ python/diamond/diamond.py
@@ -1,9 +1,39 @@
 import string
 
 def rows(letter):
-    # return ["A"]
-    row_length(letter)
+    basicLine = ["A"]
 
-def row_length(letter):
-    letter_index = string.ascii_uppercase.index(letter)
-    return 2 * letter_index + 1
+    if letter == "A":
+        return basicLine
+
+    index = string.ascii_uppercase.index(letter)
+    rowLength = 2 * index + 1
+
+    # 使用されるアルファベットのリストを作成
+    useLetters = string.ascii_uppercase[0:index+1]
+
+
+    for s in useLetters[1:] :
+        basicLine.insert(0, s)
+        basicLine.append(s)
+
+    firstHalfParts = []
+
+    for charactor in useLetters:
+        diamondLine = []
+        for checkChar in basicLine:
+            if charactor != checkChar :
+                diamondLine.append(" ")
+            else:
+                diamondLine.append(charactor)
+        if charactor != letter:
+            firstHalfParts.append("".join(diamondLine))
+        else:
+            centerLine = "".join(diamondLine)
+    secondHalfParts = firstHalfParts[::-1]
+    diamond = []
+    diamond = firstHalfParts
+    diamond.append(centerLine)
+    diamond.extend(secondHalfParts)
+    return diamond
+    
