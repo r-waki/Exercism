cb8618d3a22b07d3df0d97ce518b3d1dfe4e8418
r-waki <r.wakeyii@gmail.com>
2025-02-03 16:55:59 +0900

    feat(python): ocr-numbers completed

--- python/ocr-numbers/ocr_numbers.py
+++ python/ocr-numbers/ocr_numbers.py
@@ -1,3 +1,4 @@
+from collections import deque
 
 numbers_grid = [ 
      " _ | ||_|   " ,
@@ -15,34 +16,39 @@ numbers_grid = [
 
 def convert(input_grid):
 
-    first_row = input_grid[0]
-    second_row = input_grid[1]
-    third_row = input_grid[2]
-    fourth_row = input_grid[3]
-
     check_grid_number(input_grid)
-    check_columns_number(first_row)
-    check_columns_number(second_row)
-    check_columns_number(third_row)
-    check_columns_number(fourth_row)
-
-    char_count = int(len(input_grid[0]) / 3)
+    que_grid = deque(input_grid)
     chars = ""
 
-    for x in range(char_count):
-        char = ""
-        char += first_row[ 3*x : 3*(x+1) ]
-        char += second_row[ 3*x : 3*(x+1) ]
-        char += third_row[ 3*x : 3*(x+1) ]
-        char += fourth_row[ 3*x : 3*(x+1) ]
-    
-        iter_numbers_grid = iter(numbers_grid)
-        for i, y in enumerate(iter_numbers_grid):
-            if y == char:
-                chars += str(i)
-                break
-        else:
-            chars += "?"
+    while que_grid:
+        first_row = que_grid.popleft()
+        second_row = que_grid.popleft()
+        third_row = que_grid.popleft()
+        fourth_row = que_grid.popleft()
+
+        check_columns_number(first_row)
+        check_columns_number(second_row)
+        check_columns_number(third_row)
+        check_columns_number(fourth_row)
+
+        char_count = int(len(input_grid[0]) / 3)
+
+        for x in range(char_count):
+            char = ""
+            char += first_row[ 3*x : 3*(x+1) ]
+            char += second_row[ 3*x : 3*(x+1) ]
+            char += third_row[ 3*x : 3*(x+1) ]
+            char += fourth_row[ 3*x : 3*(x+1) ]
+
+            iter_numbers_grid = iter(numbers_grid)
+            for i, y in enumerate(iter_numbers_grid):
+                if y == char:
+                    chars += str(i)
+                    break
+            else:
+                chars += "?"
+        if que_grid:
+            chars += ","
     return chars
 
 def check_grid_number(input_grid):
